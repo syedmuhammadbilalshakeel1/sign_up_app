58c0055b7ad9655ead9b214ac3ff4bc9909537ab
syed muhammad bilal <syedmuhammadbilal638@gmail.com>
2022-10-04 11:20:38 +0500

    new js

---
+++ app.js
@@ -0,0 +1,12 @@
+// var let const
+// var name = "umar"
+// function foo(){
+// if (true){
+//    let fullname = "bilal";
+// }
+// foo()
+var lastname ="ali";
+var lastname ="usama";
+
+console.log(lastname);
+
