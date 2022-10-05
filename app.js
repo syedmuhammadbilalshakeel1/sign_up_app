cc012f1e9ee4fa790d777651fb301dc6d36243cb
syed muhammad bilal <syedmuhammadbilal638@gmail.com>
2022-10-05 21:01:59 +0500

    new comment

--- app.js
+++ app.js
@@ -10,3 +10,18 @@ var lastname ="usama";
 
 console.log(lastname);
 
+
+var student1 =["umar" ,"ali"];
+var student2 =[...student1 ,"bilal" ,"Muhammad"];
+
+console.log(student2)
+
+// var mer = student1.concat(student2);
+// console.log(mer)
+
+setTimeout(function (){
+console.log("number 1")
+},1000);
+
+console.log("number 2");
+console.log("number 3");
